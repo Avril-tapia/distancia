1506d41838d75674957dd35b1328c79747764f31
Avril-tapia <taaf080709mgtplta1@cbtis217.edu.mx>
2024-10-14 15:48:46 +0200

    Mi primer commit

---
+++ cubeta.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[7];
+int main()
+{
+    int n,x;
+    cin>>n;
+    ///LEER DATOS EN LA CUBETA
+    for(int i=1; i<=n; i++)
+    {
+        cin>>x;
+        a[x]++;
+    }
+    ///IMPRIMIR CUÁNTOS HAY EN CADA SEMESTRE
+    cout<<"Semestre 1: "<<a[1]<<endl;
+    cout<<"Semestre 2: "<<a[2]<<endl;
+    cout<<"Semestre 3: "<<a[3]<<endl;
+    cout<<"Semestre 4: "<<a[4]<<endl;
+    cout<<"Semestre 5: "<<a[5]<<endl;
+    cout<<"Semestre 6: "<<a[6]<<endl;
+
+    return 0;
+}
